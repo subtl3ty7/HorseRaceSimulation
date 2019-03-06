2e5e889b13930df0a2acbd576a3810828518fd15
Mehmet Ege Demirsoy <e1641187@student.tuwien.ac.at>
2019-03-06 21:48:34 +0100

    Changed from Auto_increment to sequence for better modification options when deleting a row.

--- src/main/resources/sql/createSchema.sql
+++ src/main/resources/sql/createSchema.sql
@@ -1,8 +1,18 @@
 -- create table horse if not exists
+DROP TABLE horse;
+DROP SEQUENCE seq_1;
+
+CREATE SEQUENCE seq_1
+START WITH 1
+INCREMENT BY 1
+MAXVALUE 999
+CYCLE;
+
 
 CREATE TABLE IF NOT EXISTS horse (
   -- use auto incrementing IDs as primary key
-  id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+  --id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+  id        BIGINT DEFAULT seq_1.nextval PRIMARY KEY,
   name      VARCHAR(255) NOT NULL,
   breed     TEXT,
   min_speed DOUBLE       NOT NULL,
