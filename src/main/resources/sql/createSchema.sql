eccceeb4d206e0dd2cb85002aa30c6ffc3ead9d7
Mehmet Ege Demirsoy <e1641187@student.tuwien.ac.at>
2019-03-08 14:35:46 +0100

    Jockey Table added in Schema. Deleted Sequences to revert the program to its old state.

--- src/main/resources/sql/createSchema.sql
+++ src/main/resources/sql/createSchema.sql
@@ -1,22 +1,25 @@
 -- create table horse if not exists
 DROP TABLE horse;
-DROP SEQUENCE seq_1;
-
-CREATE SEQUENCE seq_1
-START WITH 1
-INCREMENT BY 1
-MAXVALUE 999
-CYCLE;
+DROP TABLE jockey;
 
 
 CREATE TABLE IF NOT EXISTS horse (
   -- use auto incrementing IDs as primary key
-  --id        BIGINT AUTO_INCREMENT PRIMARY KEY,
-  id        BIGINT DEFAULT seq_1.nextval PRIMARY KEY,
+  id        BIGINT AUTO_INCREMENT PRIMARY KEY,
   name      VARCHAR(255) NOT NULL,
   breed     TEXT,
   min_speed DOUBLE       NOT NULL,
   max_speed DOUBLE       NOT NULL,
   created   DATETIME     NOT NULL,
   updated   DATETIME     NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS jockey (
+
+  id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+  name      VARCHAR(255) NOT NULL,
+  skill     DOUBLE       NOT NULL,
+  created   DATETIME     NOT NULL,
+  updated   DATETIME     NOT NULL
+
 );
