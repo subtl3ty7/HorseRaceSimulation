dec8f8b290401509c77d77d6de4aecf6fe915cab
e1641187 <e1641187@student.tuwien.ac.at>
2019-03-12 13:14:06 +0100

    Implemented simulation mapper.

---
+++ src/main/java/at/ac/tuwien/sepm/assignment/individual/util/mapper/SimulationMapper.java
@@ -0,0 +1,45 @@
+package at.ac.tuwien.sepm.assignment.individual.util.mapper;
+
+import at.ac.tuwien.sepm.assignment.individual.entity.Simulation;
+import at.ac.tuwien.sepm.assignment.individual.rest.dto.SimulationDto;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SimulationMapper {
+
+    /**
+     * Changes a simulation entity to dto.
+     *
+     * @param simulation
+     * @return value is a DTO.
+     */
+    public SimulationDto entityToDto(Simulation simulation) {
+        return new SimulationDto(simulation.getName(), simulation.getSimulationParticipants(), simulation.getCreated());
+    }
+
+    /**
+     * Changes a simulation dto to entity.
+     *
+     * @param simulation
+     * @return is an entity.
+     */
+    public Simulation dtoToEntity (SimulationDto simulation) {
+        return new Simulation(simulation.getName(), simulation.getSimulationParticipants(), simulation.getCreated());
+    }
+
+    /**
+     * Changes a simulation entity list to dto list.
+     *
+     * @param simulationList
+     * @return value is a list of simulation DTOs.
+     */
+    public List<SimulationDto> listEntityToDTO(List<Simulation> simulationList){
+        List<SimulationDto> simulationDtos = new LinkedList<>();
+        for (Simulation simulation : simulationList) {
+            simulationDtos.add(entityToDto(simulation));
+        }
+        return simulationDtos;
+    }
+
+
+}
