db1ade6fb91eb20177c7c7fe09ed3171ad767e71
Mehmet Ege Demirsoy <e1641187@student.tuwien.ac.at>
2019-03-14 16:12:15 +0100

    Wrote the methods for getting simulations(by Id, by Name Pattern, All)

--- src/main/java/at/ac/tuwien/sepm/assignment/individual/service/ISimulationService.java
+++ src/main/java/at/ac/tuwien/sepm/assignment/individual/service/ISimulationService.java
@@ -5,17 +5,64 @@ import at.ac.tuwien.sepm.assignment.individual.entity.SimulationResult;
 import at.ac.tuwien.sepm.assignment.individual.exceptions.NotFoundException;
 import at.ac.tuwien.sepm.assignment.individual.service.exceptions.ServiceException;
 
+import java.util.List;
+
 public interface ISimulationService {
 
+
+    SimulationResult getOneById( Integer id) throws ServiceException,NotFoundException;
+
+    /**
+     *
+     * @param simulation
+     * @return
+     * @throws ServiceException
+     * @throws NotFoundException
+     */
     SimulationResult insertSimulationToDB(SimulationInput simulation) throws ServiceException, NotFoundException;
 
+    List<SimulationResult> getAllSimulations() throws ServiceException, NotFoundException;
+
+    List<SimulationResult> getAllSimulationsFiltered(String name) throws ServiceException, NotFoundException;
+
+    /**
+     *
+     * @param simulation
+     * @return
+     * @throws ServiceException
+     * @throws NotFoundException
+     */
     SimulationResult simulate(SimulationInput simulation) throws ServiceException,NotFoundException;
 
+    /**
+     *
+     * @param horseSpeed
+     * @param jockeySkill
+     * @param luckFactor
+     * @return
+     */
     Double calculateAvgSpeed(Double horseSpeed, Double jockeySkill, Double luckFactor);
 
+    /**
+     *
+     * @param skill
+     * @return
+     */
     Double calculateJockeySkill(Double skill);
 
+    /**
+     *
+     * @param horseMin
+     * @param horseMax
+     * @param luckFactor
+     * @return
+     */
     Double calculateSpeed (Double horseMin, Double horseMax, Double luckFactor);
 
+    /**
+     *
+     * @param input
+     * @return
+     */
     Double truncateAndRound(Double input);
 }
