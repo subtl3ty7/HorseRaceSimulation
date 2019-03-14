a21b99c7b3490eb771d2d1293d76888b57064585
Mehmet Ege Demirsoy <e1641187@student.tuwien.ac.at>
2019-03-14 16:12:51 +0100

    Wrote the methods for getting simulations(by Id, by Name Pattern, All)

--- src/main/java/at/ac/tuwien/sepm/assignment/individual/persistence/ISimulationDao.java
+++ src/main/java/at/ac/tuwien/sepm/assignment/individual/persistence/ISimulationDao.java
@@ -1,9 +1,14 @@
 package at.ac.tuwien.sepm.assignment.individual.persistence;
 
 import at.ac.tuwien.sepm.assignment.individual.entity.SimulationResult;
+import at.ac.tuwien.sepm.assignment.individual.exceptions.NotFoundException;
 import at.ac.tuwien.sepm.assignment.individual.persistence.exceptions.PersistenceException;
 
-public interface ISimulationDao {
+import java.util.List;
 
+public interface ISimulationDao {
+    SimulationResult getOneById (Integer id) throws PersistenceException;
     SimulationResult insertSimulationResultToDB(SimulationResult simulationResult) throws PersistenceException;
+    List<SimulationResult> getAllSimulations() throws PersistenceException, NotFoundException;
+    List<SimulationResult> getAllSimulationsFiltered(String name) throws PersistenceException, NotFoundException;
 }
