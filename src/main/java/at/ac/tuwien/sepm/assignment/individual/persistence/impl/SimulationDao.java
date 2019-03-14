4b94184e8197300aea457f0df292e0eacc496f13
Mehmet Ege Demirsoy <e1641187@student.tuwien.ac.at>
2019-03-14 16:13:08 +0100

    Implemented the methods for getting simulations(by Id, by Name Pattern, All)

--- src/main/java/at/ac/tuwien/sepm/assignment/individual/persistence/impl/SimulationDao.java
+++ src/main/java/at/ac/tuwien/sepm/assignment/individual/persistence/impl/SimulationDao.java
@@ -3,6 +3,7 @@ package at.ac.tuwien.sepm.assignment.individual.persistence.impl;
 
 import at.ac.tuwien.sepm.assignment.individual.entity.HorseJockeyCombination;
 import at.ac.tuwien.sepm.assignment.individual.entity.SimulationResult;
+import at.ac.tuwien.sepm.assignment.individual.exceptions.NotFoundException;
 import at.ac.tuwien.sepm.assignment.individual.persistence.ISimulationDao;
 import at.ac.tuwien.sepm.assignment.individual.persistence.exceptions.PersistenceException;
 import at.ac.tuwien.sepm.assignment.individual.persistence.util.DBConnectionManager;
@@ -15,6 +16,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class SimulationDao implements ISimulationDao {
@@ -25,6 +28,36 @@ public class SimulationDao implements ISimulationDao {
     @Autowired
     public SimulationDao(DBConnectionManager dbConnectionManager) {
         this.dbConnectionManager = dbConnectionManager;
+    }
+
+
+    @Override
+    public SimulationResult getOneById (Integer id) throws PersistenceException {
+        String sql = "SELECT * FROM Simulation WHERE id = ?";
+        SimulationResult simulationResult = new SimulationResult(id,null,null,null);
+        try{
+            PreparedStatement statement = dbConnectionManager.getConnection().prepareStatement(sql);
+            ResultSet result = statement.executeQuery();
+            List<HorseJockeyCombination> horseJockeyCombinations = new ArrayList<>();
+
+            while(result.next()){
+                simulationResult.setName(result.getString("name"));
+                simulationResult.setCreated(result.getTimestamp("created").toLocalDateTime());
+                horseJockeyCombinations.add(new HorseJockeyCombination(result.getInt("rank"),
+                    result.getString("horseName"),result.getString("jockeyName"),result.getDouble("avgSpeed"),
+                    result.getDouble("horseSpeed"),result.getDouble("skill"),result.getDouble("luckFactor")));
+            }
+
+            simulationResult.setHorseJockeyCombinations(horseJockeyCombinations);
+
+
+        }
+        catch (SQLException e){
+            LOGGER.error("Problem while executing SQL statement for getting Simulation with id: " + id, e);
+            throw new PersistenceException("Could not get simulation " + id,e);
+        }
+        return simulationResult;
+
     }
 
 
@@ -81,11 +114,76 @@ public class SimulationDao implements ISimulationDao {
 
 
         } catch(SQLException e){
-
+            LOGGER.error("Problem while executing SQL statement for inserting simulation", e);
+            throw new PersistenceException("Could not insert simulation",e);
         }
 
         simulationResult.setId(id);
         return simulationResult;
     }
 
+
+    @Override
+    public List<SimulationResult> getAllSimulations() throws PersistenceException,NotFoundException{
+        LOGGER.info("Get all Simulations");
+        String sql = "SELECT TOP 1 * FROM Simulation \n" +
+            "ORDER BY Id DESC";
+        int highestId = 0;
+        try {
+            PreparedStatement statementCheck = dbConnectionManager.getConnection().prepareStatement(sql);
+            ResultSet result;
+            result = statementCheck.executeQuery();
+            while(result.next()){
+                highestId = result.getInt(1);
+                if(highestId == 0){
+                    throw new NotFoundException("Simulation table is empty.");
+                }
+            }
+            List<SimulationResult> simulationResults = new ArrayList<>();
+
+            for (int i = 1; i <= highestId;i++){
+                simulationResults.add(getOneById(i));
+            }
+
+            return simulationResults;
+
+        } catch (SQLException e) {
+            LOGGER.error("Problem while executing SQL statement for getting all simulations", e);
+            throw new PersistenceException("Could not get simulations",e);
+        }
+    }
+
+
+    @Override
+    public List<SimulationResult> getAllSimulationsFiltered(String name) throws PersistenceException, NotFoundException {
+        LOGGER.info("Get all Simulations filtered with: " + name);
+        String sql = "SELECT * FROM Simulation WHERE name LIKE ?";
+        List<SimulationResult> simulationResults = new ArrayList<>();
+        try {
+            PreparedStatement statementCheck = dbConnectionManager.getConnection().prepareStatement(sql);
+            statementCheck.setString(1,"%" + name + "%");
+            ResultSet result = statementCheck.executeQuery();
+            List<Integer> ids = new ArrayList<>();
+            while(result.next()){
+                Integer id  = result.getInt(1);
+                if(id == 0){
+                    throw new NotFoundException("Simulation table is empty.");
+                }
+                if(!ids.contains(id)){
+                    ids.add(id);
+                }
+            }
+
+            for (Integer id: ids) {
+                simulationResults.add(getOneById(id));
+            }
+
+
+        }catch (SQLException e){
+            LOGGER.error("Problem while executing SQL statement for getting all simulations", e);
+            throw new PersistenceException("Could not get filtered simulations",e);
+        }
+
+        return simulationResults;
+    }
 }
