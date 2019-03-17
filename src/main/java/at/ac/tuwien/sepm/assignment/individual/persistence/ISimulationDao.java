0e8c6a00fef87aec7ff16b7abfa3adf38256e3a5
Mehmet Ege Demirsoy <e1641187@student.tuwien.ac.at>
2019-03-17 19:18:51 +0100

    Documentation partially finished

--- src/main/java/at/ac/tuwien/sepm/assignment/individual/persistence/ISimulationDao.java
+++ src/main/java/at/ac/tuwien/sepm/assignment/individual/persistence/ISimulationDao.java
@@ -7,7 +7,21 @@ import at.ac.tuwien.sepm.assignment.individual.persistence.exceptions.Persistenc
 import java.util.List;
 
 public interface ISimulationDao {
+
+    /**
+     * Returns a simulation by its id.
+     * @param id is the id of the searched simulation.
+     * @return is the SimulationResult Object.
+     * @throws PersistenceException will be thrown if something goes wrong during the database access.
+     */
     SimulationResult getOneById (Integer id) throws PersistenceException;
+
+    /**
+     * Inserts a finished simulation with its results to database.
+     * @param simulationResult  is the finished simulation to insert.
+     * @return is the inserted simulation.
+     * @throws PersistenceException will be thrown if something goes wrong during the database access.
+     */
     SimulationResult insertSimulationResultToDB(SimulationResult simulationResult) throws PersistenceException;
     List<SimulationResult> getAllSimulations() throws PersistenceException, NotFoundException;
     List<SimulationResult> getAllSimulationsFiltered(String name) throws PersistenceException, NotFoundException;
