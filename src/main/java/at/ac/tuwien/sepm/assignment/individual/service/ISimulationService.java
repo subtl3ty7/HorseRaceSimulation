ce07db1aac979e60980f397db466dccaf87a8ae5
Mehmet Ege Demirsoy <e1641187@student.tuwien.ac.at>
2019-03-22 20:02:28 +0100

    Doucmentation finished.

--- src/main/java/at/ac/tuwien/sepm/assignment/individual/service/ISimulationService.java
+++ src/main/java/at/ac/tuwien/sepm/assignment/individual/service/ISimulationService.java
@@ -9,24 +9,49 @@ import java.util.List;
 
 public interface ISimulationService {
 
+    /**
+     * Returns a simulation from its id.
+     *
+     * @param id is the id of the searched simulation
+     * @return value is the SimulationResult
+     * @throws ServiceException will be thrown if something goes wrong during the data processing.
+     * @throws NotFoundException will be thrown if there is no simulations in the system.
+     */
 
     SimulationResult getOneById( Integer id) throws ServiceException,NotFoundException;
 
     /**
+     *Inserts a simulation into database and calculates the simulation result in this layer.
+     * SimulationInput to SimulationResult conversion happens here.
      *
-     * @param simulation
-     * @return
-     * @throws ServiceException
-     * @throws NotFoundException
+     * @param simulation is the simulation to be inserted.
+     * @return is the result of the inserted simulation.
+     * @throws ServiceException will be thrown if something goes wrong during the data processing.
+     * @throws NotFoundException will be thrown if there is no simulations in the system.
      */
     SimulationResult insertSimulationToDB(SimulationInput simulation) throws ServiceException, NotFoundException;
 
+    /**
+     * Returns all the simulations in database.
+     *
+     * @return value is a list of simulations in database.
+     * @throws ServiceException will be thrown if something goes wrong during the data processing.
+     * @throws NotFoundException will be thrown if there is no simulations in the system.
+     */
     List<SimulationResult> getAllSimulations() throws ServiceException, NotFoundException;
 
+    /**
+     * Returns all the simulations passing a desired filter in database.
+     * @param name is the name pattern to filter out the simulations.
+     * @return value is a list of filtered simulations.
+     * @throws ServiceException will be thrown if something goes wrong during the data processing.
+     * @throws NotFoundException will be thrown if there is no simulations in the system.
+     */
     List<SimulationResult> getAllSimulationsFiltered(String name) throws ServiceException, NotFoundException;
 
     /**
      *
+     * The main method to run simulation and calculate the results.
      * @param simulation
      * @return
      * @throws ServiceException
@@ -35,34 +60,35 @@ public interface ISimulationService {
     SimulationResult simulate(SimulationInput simulation) throws ServiceException,NotFoundException;
 
     /**
-     *
-     * @param horseSpeed
-     * @param jockeySkill
-     * @param luckFactor
-     * @return
+     * For calculating horse's average speed.
+     * @param horseSpeed is the speed of the horse
+     * @param jockeySkill is the skill of the jockey
+     * @param luckFactor is the luck factor.
+     * @return value is the calculated average speed.
      */
     Double calculateAvgSpeed(Double horseSpeed, Double jockeySkill, Double luckFactor);
 
     /**
+     * For calculating jockey's skill.
      *
-     * @param skill
-     * @return
+     * @param skill is the jockey's default skill value.
+     * @return value is the calculated skill value.
      */
     Double calculateJockeySkill(Double skill);
 
     /**
-     *
-     * @param horseMin
-     * @param horseMax
-     * @param luckFactor
-     * @return
+     * For calculating horse's speed.
+     * @param horseMin is the minimum speed of the horse.
+     * @param horseMax is the maximum speed of the horse.
+     * @param luckFactor is the luck factor.
+     * @return value is the speed of the horse.
      */
     Double calculateSpeed (Double horseMin, Double horseMax, Double luckFactor);
 
     /**
-     *
-     * @param input
-     * @return
+     * Truncates a value and depending on condition, rounds it up or down
+     * @param input is the value to be truncated and rounded.
+     * @return value is the truncated and rounded value.
      */
     Double truncateAndRound(Double input);
 }
